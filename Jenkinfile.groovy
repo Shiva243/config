5ea1244c37af213fa8d78e81457961b73b74960e
Shivakoti <shivav809@gmail.com>
2017-02-22 18:47:11 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -1,7 +1,9 @@
 def devStaging() {
     stage 'checkout from git'{
         echo 'test';
+        steps{
          git credentialsId: 'a1a2265a-6c99-43cd-941d-62ee5b4b10e1', url: 'https://github.com/Shiva243/FIM.git'
+        }
     }
     stage 'Genrating the war file'{
          sh 'gradle clean build'
