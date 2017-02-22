bea2e9ef028c64ecf9e9845ee071043bb3fe97c7
Shivakoti <shivav809@gmail.com>
2017-02-22 23:04:27 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -1,9 +1,13 @@
 node{
+    
     stage 'Test'
         echo 'test'
     stage 'check out'
         git url:'https://github.com/Shiva243/config.git'
         echo 'Successfully checkout'
+    stage 'Build project'
+        sh 'gradle clean build'
+        echo 'build successfully'
     
             
 }
