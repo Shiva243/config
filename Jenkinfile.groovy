1a97188441eafdc12fa806357669e5cd7266572d
Shivakoti <shivav809@gmail.com>
2017-02-23 19:29:57 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -10,7 +10,7 @@ node{
         sh 'gradle clean build'
         echo 'build successfully'
         currentBuild.result='build successfully'
-        currentBuild.result
+        
         stage 'deploy'
             sh 'cf login -a https://api.run.pivotal.io -u svelagandula@nisum.com -p Neethu@243'
             sh 'cf push FIMTEST'
