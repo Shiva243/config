d1c008607f027ce29adb5fa2cd4f139534ff8b65
Shivakoti <shivav809@gmail.com>
2017-02-23 22:29:56 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -28,8 +28,7 @@ def notifyBuild(String buildStatus){
 def markup = new groovy.xml.MarkupBuilder(writer) 
     echo "build status '${buildStatus}'"
     mail  subject: "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' ", 
-        bcc: '', body: "${buildStatus} \n Job Details
-    '${env.JOB_NAME} [${env.BUILD_NUMBER}] (${env.BUILD_URL})'", 
+        bcc: '', body: "${buildStatus} \n Job Details '${env.JOB_NAME} [${env.BUILD_NUMBER}] (${env.BUILD_URL})'", 
         cc: '', from: '', replyTo: '',
         to: 'shivav809@gmail.com'
 }
