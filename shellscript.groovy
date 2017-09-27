499f0f738bcfc7d14de41ffe8ea56acc1c378475
Shivakoti <shivav809@gmail.com>
2017-09-27 14:28:55 +0530

    Update shellscript.groovy

--- shellscript.groovy
+++ shellscript.groovy
@@ -6,13 +6,14 @@ node{
         git url:'https://github.com/Shiva243/config.git'
         echo 'Successfully checkout'
     stage 'Build project'
-   
+   dir(${WORKSPACE}){
       def sout = new StringBuffer(), serr = new StringBuffer()
       
       def proc = '/script.sh'.execute()
       proc.consumeProcessOutput(sout, serr)
       proc.waitForOrKill(1000)
       println sout
+   }
 }
 
 
